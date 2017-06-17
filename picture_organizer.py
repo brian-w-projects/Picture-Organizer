d5b08d04a144782b472c6925b7ead3c833423f14
Brian W <brian.weinfeld@gmail.com>
2017-06-17 17:26:37 -0400

    added single folder option

--- picture_organizer.py
+++ picture_organizer.py
@@ -1,17 +1,16 @@
 from os import chdir, listdir, getcwd, rename, walk
-from os.path import isdir, split, isfile, splitext, join
+from os.path import isdir, split, isfile, splitext, join, dirname, basename
 from math import log, ceil
 import argparse
 
 
-def rename_folder(working_path, base_name='', skip_list=()):
+def rename_folder(working_path, base_name, skip_list=()):
     print('Working {}'.format(working_path))
     if isdir(working_path):
         chdir(working_path)
-        base_name = base_name or split(getcwd())[-1].replace(' ', '_')
         files = [file for file in listdir(working_path) if isfile(file) and splitext(file)[-1] not in skip_list
                  and not file.startswith('.')]
-        digits = int(ceil(log(len(files),10))) if len(files) > 0 else 0
+        digits = ceil(log(len(files), 10)) if len(files) > 0 else 0
         for i in range(len(files)):
             file_name, file_ext = splitext(files[i])
             if file_ext not in skip_list:
@@ -25,30 +24,43 @@ def rename_folder(working_path, base_name='', skip_list=()):
 
 def rename_folders(working_path, identify, skip_list, folder_skip_list):
     name_changes = {}
-    for root, dirs, files in walk(working_path):
+    for root, dirs, _ in walk(working_path):
         for d in [x for x in dirs if x not in folder_skip_list and not x.startswith('.')]:
-            full_path = join(root, d)
-            if identify:
-                new_name = input('"{}" should be labeled as: '.format(full_path)).replace(' ', '_')
-                if new_name:
-                    name_changes[full_path] = new_name
-                else:
-                    print('Skipping {}'.format(full_path))
-            else:
-                name_changes[full_path] = d.lower()
+            new_name = naming(root, d, identify)
+            if new_name:
+                name_changes[join(root, d)] = new_name
     for full_path in name_changes.keys():
         rename_folder(full_path, base_name=name_changes[full_path], skip_list=skip_list)
 
+
+def naming(root, d, identify):
+    full_path = join(root, d)
+    if identify:
+        new_name = input('"{}" should be labeled as: '.format(full_path)).replace(' ', '_')
+        if new_name:
+            return new_name
+        else:
+            print('Skipping "{}"'.format(full_path))
+            return
+    else:
+        return d.lower().replace(' ', '_')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Rename files in folders for consistent naming schemes.')
     parser.add_argument('pathway', action='store', help='Folder pathway to modify. May contain other folders.')
     parser.add_argument('-i', action='store_true', dest='identify', default=False,
-                        help='Identify file naming scheme. Default will use containg folder name.')
+                        help='Identify file naming scheme. Default will use containing folder name.')
     parser.add_argument('-s', nargs='*', dest='skip', action='store',
                         help='List of extensions to skip')
     parser.add_argument('-f', nargs='*', dest='folder_skip', action='store',
                         help='List of folders to skip')
+    parser.add_argument('-r', dest='recurse', action='store_true', default=False,
+                        help='Identify whether to recurse through inner folders')
     results = parser.parse_args()
 
-    rename_folders(results.pathway, results.identify, results.skip or [], results.folder_skip or [])
+    if results.recurse:
+        rename_folders(results.pathway, results.identify, results.skip or [], results.folder_skip or [])
+    else:
+        new_name = naming(dirname(results.pathway), basename(results.pathway), results.identify)
+        if new_name:
+            rename_folder(results.pathway, new_name, results.skip or [])
